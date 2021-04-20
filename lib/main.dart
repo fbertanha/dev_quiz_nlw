afd410c1729cfd587b3dd2bcccb75b62c1793996
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2021-04-20 13:21:00 -0300

    class 1 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dev_quiz_nlw/home/home_page.dart';
+import 'package:dev_quiz_nlw/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo');
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
   }
 }
